7b6c1496a77d1e2495396cc955d2aacc4bbc5355
BakurovKM <bakurovkm@sibur.ru>
2023-01-19 16:46:27 +0300

    add getting rates in CSV

--- src/controllers/rates.controller.js
+++ src/controllers/rates.controller.js
@@ -15,7 +15,21 @@ const getAll = async (req, res) => {
     res.json(result);
 }
 
+const getAllInCSV = async (req, res) => {
+    let rates = await rateService.getAll();
+    let csv = "Name;Code;Rate\n";
+
+    for (key in rates){
+        csv += `${rates[key].Name};${rates[key].CharCode};${rates[key].Value}\n`;
+    }
+
+    res.setHeader('Content-disposition', 'attachment; filename=rates.csv');
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(csv);
+}
+
 module.exports = {
     getAll,
-    getByCurrencyCode
+    getByCurrencyCode,
+    getAllInCSV
 };
--- src/routes/rates.route.js
+++ src/routes/rates.route.js
@@ -5,6 +5,7 @@ const asyncHandler = require('express-async-handler')
 const controller = require('../controllers/rates.controller');
 
 router.get('/', asyncHandler(async (req, res, next) => await controller.getAll(req, res)));
+router.get('/download/csv', asyncHandler(async (req, res, next) => await controller.getAllInCSV(req, res)));
 router.get('/:code', asyncHandler(async (req, res, next) => await controller.getByCurrencyCode(req, res)));
 
 module.exports = router;
